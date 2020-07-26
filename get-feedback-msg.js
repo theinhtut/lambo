e64d3fb2b2783fd57a2694d6d22cc3467869acea
Thein Htut <theinhtut.dec@gmail.com>
2020-07-26 22:23:48 +0800

    Added locally invoke lambda func on docker

--- get-feedback-msg.js
+++ get-feedback-msg.js
@@ -1,12 +1,25 @@
+'use strict'
+const AWS = require('aws-sdk')
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10',
+  endpoint: 'http://host.docker.internal:8000'
+})
+
 let response
 
 exports.lambdaHandler = async (event, context) => {
+  // Make params
+  const params = {
+    TableName: process.env.DDB_TABLE_NAME
+  }
+
+  // Scan ddb table and return one or more items
   try {
+    const data = await docClient.scan(params).promise()
     response = {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
+      body: JSON.stringify(data.Items)
     }
   } catch (err) {
     console.log(err)
